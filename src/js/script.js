4188fb0c999657f483cf572398455bfbd084ba37
Artem <0flacks6@gmail.com>
2020-03-21 15:35:45 +0300

    modified

--- src/js/script.js
+++ src/js/script.js
@@ -10,7 +10,7 @@ let persone = {
     age: "21",
     isMarried: false
 };
-console.log(persone['name']);
+console.log(person['name']);
 
 let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
 console.log(arr[1])
