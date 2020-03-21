4161ebf72dcddbfcab28f683718eba3c7cb1b686
Artem <0flacks6@gmail.com>
2020-03-21 15:29:29 +0300

    This is our first commit

---
+++ src/js/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var leftBorderWidth = 1;
+let socond = 2;
+const pi = 3.14;
+
+console.log(leftBorderWidth);
+let persone = {
+    name: "John",
+    age: "21",
+    isMarried: false
+};
+console.log(persone['name']);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[1])
+
+// alert("Hello, World")
+
+// let answer = confirm("Are you okey?");
+// console.log(answer);
+
+// let answer = +prompt("How old are you?", "Yes");
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+// console.log(7 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5 % 2);
+console.log("2" == 2);
+
+let isCheked = true,
+    isClose = false;
+
+console.log(isCheked && isClose);
